d0dd73c15d68b980ea43e41d25da318c73e1802f
ArpitNegi11 <arpit2802negi@gmail.com>
2025-07-14 18:38:14 +0530

    L5_CSS_JS

---
+++ L5_CSS_JS/script.js
@@ -0,0 +1,16 @@
+// var a = 10;
+// console.log(a);
+// var sum = (a,b)=>{
+//     return a+b
+
+// }
+// let ans = sum(10,20)
+// console.log(ans);
+
+let arr = [10,20,30,40]
+// console.log(arr)
+
+let newArr = arr
+newArr.push(50,60)
+console.log(newArr);
+
